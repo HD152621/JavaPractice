39bf9a75c6aae21aaff3f8e02c3d9fcc42ebbd0b
HD152621 <ymh3693@naver.com>
2017-05-16 10:49:35 +0900

    asd

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+* <pre>
+* org.dimigo.inheritance
+*	|_ SmartPhoneTest
+* 
+* 1. 개요 : 
+* 2. 작성일 : 2017. 5. 16.
+*<pre>
+*
+*@author         : USER
+*@version        : 1.0
+*/
+public class SmartPhoneTest {
+	public static void main(String[] args){
+		SmartPhone[] phones = {
+				new IPhone("IPhone 7","애플",900000),
+				new Galaxy("갤럭시 S8","삼성",800000),
+		};
+		for(int i=0; i<2; i++){
+			System.out.println(phones[i].toString());
+			phones[i].turnOn();
+			phones[i].pay();
+			phones[i].useSpecialFunction(phones[i]);
+			phones[i].turnOff();
+		}
+	}
+
+}
